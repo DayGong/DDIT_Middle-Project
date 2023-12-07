e0e87bbada763cc88dcf362770645460e41b238c
PC-19 <PC-19@DESKTOP-5IMNMP3>
2023-12-07 21:39:35 +0900

    헤더 준비

---
+++ middleProject/WebContent/js/header.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+$(() => { //마우스 올리면 메뉴 나오는 스크립트
+ 
+    $('.menu').hover(function() {
+    	
+    	  $('#menu ul ul a').css('opacity', '0');
+          $('a',this).css('opacity', '1');
+ 
+        $('#menu ul ul').css('display', 'block');
+       
+    }, function() {
+      
+        $('#menu ul ul').css('display', 'none');
+    });
+});
