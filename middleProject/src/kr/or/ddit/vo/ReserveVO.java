067b4ccf4582f37ed3af1905f3ed5ebf4e683e1f
406PC-19 <406PC-19@PC-18>
2023-12-08 12:20:17 +0900

    예약 VO 생성

---
+++ middleProject/src/kr/or/ddit/vo/ReserveVO.java
@@ -0,0 +1,11 @@
+package kr.or.ddit.vo;
+
+public class ReserveVO {
+	private int rsv_no;
+	private String rsv_name;
+	private String rsv_date;
+	private int rsv_count;
+	private int rsv_state;
+	private String mem_id;
+	private int comp_no;
+}
