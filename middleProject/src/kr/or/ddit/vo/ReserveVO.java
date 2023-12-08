731c885fb6d70cf9630af8fcbd50fe6fe351e52c
406PC-19 <406PC-19@PC-18>
2023-12-08 12:42:55 +0900

    예약VO 생성

--- middleProject/src/kr/or/ddit/vo/ReserveVO.java
+++ middleProject/src/kr/or/ddit/vo/ReserveVO.java
@@ -6,6 +6,63 @@ public class ReserveVO {
 	private String rsv_date;
 	private int rsv_count;
 	private int rsv_state;
-	private String mem_id;	// 회원가입 
-	private int comp_no;
+	private String mem_id; 		// 회원 테이블의 기본키_회원아이디
+	private int comp_no; 		// 업체 테이블의 기본키_업체번호
+
+	public int getRsv_no() {
+		return rsv_no;
+	}
+
+	public void setRsv_no(int rsv_no) {
+		this.rsv_no = rsv_no;
+	}
+
+	public String getRsv_name() {
+		return rsv_name;
+	}
+
+	public void setRsv_name(String rsv_name) {
+		this.rsv_name = rsv_name;
+	}
+
+	public String getRsv_date() {
+		return rsv_date;
+	}
+
+	public void setRsv_date(String rsv_date) {
+		this.rsv_date = rsv_date;
+	}
+
+	public int getRsv_count() {
+		return rsv_count;
+	}
+
+	public void setRsv_count(int rsv_count) {
+		this.rsv_count = rsv_count;
+	}
+
+	public int getRsv_state() {
+		return rsv_state;
+	}
+
+	public void setRsv_state(int rsv_state) {
+		this.rsv_state = rsv_state;
+	}
+
+	public String getMem_id() {
+		return mem_id;
+	}
+
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+
+	public int getComp_no() {
+		return comp_no;
+	}
+
+	public void setComp_no(int comp_no) {
+		this.comp_no = comp_no;
+	}
+
 }
