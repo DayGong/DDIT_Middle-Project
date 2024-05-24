c7cedb91620d83e6d2e27514584711611879d53d
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 01:18:15 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/tour/controller/TourUpdate.java
+++ middleProject/src/kr/or/ddit/tour/controller/TourUpdate.java
@@ -11,45 +11,38 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.beanutils.BeanUtils;
 
-
 import kr.or.ddit.tour.service.ITourService;
 import kr.or.ddit.tour.service.TourServiceImpl;
 import kr.or.ddit.vo.TourVO;
 
-
 @WebServlet("/tour/tourUpdate.do")
 public class TourUpdate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    TourVO vo= new TourVO();
-		   try {
-				BeanUtils.populate(vo, request.getParameterMap());
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			   
-			   //service 객체 얻기
-			    ITourService service= TourServiceImpl.getInstance();
-			    
-			   //service 메소드 호출 -결과값 int
-			   int cnt= service.updateTour(vo);
-			   //request에 저장
-			   request.setAttribute("result", cnt);
-			   //view페이지로 이동
-			   
-			   request.getRequestDispatcher("tourview/result.jsp").forward(request, response);
+		TourVO vo= new TourVO();
+		try {
+			BeanUtils.populate(vo, request.getParameterMap());
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		
+		//service 객체 얻기
+		ITourService service= TourServiceImpl.getInstance();
+		
+		//service 메소드 호출 -결과값 int
+		int cnt= service.updateTour(vo);
+		//request에 저장
+		request.setAttribute("result", cnt);
+		
+		//view페이지로 이동
+		request.getRequestDispatcher("tourview/result.jsp").forward(request, response);
 
 	}
 
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
