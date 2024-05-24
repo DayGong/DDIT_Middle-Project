577a9f537ae554f91279b3483c94f2b08ef918e1
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 01:19:38 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/tour/controller/WriteTour.java
+++ middleProject/src/kr/or/ddit/tour/controller/WriteTour.java
@@ -18,46 +18,40 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.beanutils.BeanUtils;
 
-
 import kr.or.ddit.tour.service.ITourService;
 import kr.or.ddit.tour.service.TourServiceImpl;
 import kr.or.ddit.vo.TourVO;
-
 
 @WebServlet("/tour/writeTour.do")
 public class WriteTour extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		  //request
+		//request
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
 		
-		  request.setCharacterEncoding("utf-8");
-		  response.setCharacterEncoding("utf-8");
-		  response.setContentType("text/html; charset=utf-8");
-		  //전송데이터받기 writer, subject, mail, content,password
-		  //vo에 저장
-		  TourVO vo= new TourVO();
-		   
-		   try {
+		//전송데이터받기 writer, subject, mail, content,password
+		TourVO vo= new TourVO();
+		
+		try {
 			BeanUtils.populate(vo, request.getParameterMap());
 		} catch (IllegalAccessException e) {
-			
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
-			
 			e.printStackTrace();
 		}
-		   
+		
 		// 서비스객체
-		   ITourService service= TourServiceImpl.getInstance();
+		ITourService service= TourServiceImpl.getInstance();
 		// 서비스메소드 호출 -결과값 받기 -int
-		  int res= service.insertTour(vo);
+		int res= service.insertTour(vo);
 		// request에 저장   		
-	      request.setAttribute("result", res);
-	    // view로 이동
-		  request.getRequestDispatcher("tourview/result.jsp").forward(request, response);
-			 
+		request.setAttribute("result", res);
+		// view로 이동
+		request.getRequestDispatcher("tourview/result.jsp").forward(request, response);
+
 	}
 
 }
