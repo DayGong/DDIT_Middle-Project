32786aec0aa0a9c0fe66f55fb34fe955f3d59805
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 01:12:53 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/resturant/service/IRestaurantService.java
+++ middleProject/src/kr/or/ddit/resturant/service/IRestaurantService.java
@@ -5,14 +5,11 @@ import java.util.List;
 import kr.or.ddit.vo.RestaurantVO;
 
 public interface IRestaurantService {
-
-
-    
+	
 	public List<RestaurantVO> selectRestaurant(); 
-
-
+	
 	public List<RestaurantVO> selectByDong(String dong);
-
-
+	
 	public List<RestaurantVO> selectByName(String name);
+	
 }
