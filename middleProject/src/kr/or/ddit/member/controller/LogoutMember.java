6395c59d47415ec90661856fe86c4f32d913584a
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 00:31:03 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/member/controller/LogoutMember.java
+++ middleProject/src/kr/or/ddit/member/controller/LogoutMember.java
@@ -1,6 +1,5 @@
 package kr.or.ddit.member.controller;
 
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -13,7 +12,6 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/member/logoutMember.do")
 public class LogoutMember extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession  session = request.getSession();
