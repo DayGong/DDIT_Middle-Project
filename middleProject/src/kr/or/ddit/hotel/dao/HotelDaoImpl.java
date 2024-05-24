9a4013cf52751dd58687292243d7b5c9cc151b31
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 00:23:33 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/hotel/dao/HotelDaoImpl.java
+++ middleProject/src/kr/or/ddit/hotel/dao/HotelDaoImpl.java
@@ -9,52 +9,54 @@ import kr.or.ddit.vo.HotelVO;
 
 public class HotelDaoImpl implements IHotelDao {
 	private static  IHotelDao dao;
-    private HotelDaoImpl() {}     
+	private HotelDaoImpl() {}     
 	public static IHotelDao getInstance() {
 		if(dao==null) dao= new HotelDaoImpl();
 		return dao;
-	    }  //singleton 패턴	
+	}  //singleton 패턴
+	
 	@Override
 	public List<HotelVO> selectHotel() {
 		List<HotelVO> list= null;
 		SqlSession session= MybatisUtil.getSqlSession();
-			try {
-				list=session.selectList("hotelVo.selectHotel");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}finally {
-					session.commit();
-					session.close();
-				}
-					return list;
+		try {
+			list=session.selectList("hotelVo.selectHotel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.commit();
+			session.close();
+		}
+		return list;
 	}//selectCulture 끝
+	
 	@Override
 	public List<HotelVO> selectByDong(String dong) {
 		List<HotelVO> list= null;
 		SqlSession session= MybatisUtil.getSqlSession();
-			try {
-				list=session.selectList("hotelVo.selectByDong",dong);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}finally {
-					session.commit();
-					session.close();
-				}
-					return list;
+		try {
+			list=session.selectList("hotelVo.selectByDong",dong);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.commit();
+			session.close();
+		}
+		return list;
 	}
+	
 	@Override
 	public List<HotelVO> selectbyName(String dong) {
 		List<HotelVO> list= null;
 		SqlSession session= MybatisUtil.getSqlSession();
-			try {
-				list=session.selectList("hotelVo.selectByName",dong);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}finally {
-					session.commit();
-					session.close();
-				}
-					return list;
+		try {
+			list=session.selectList("hotelVo.selectByName",dong);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.commit();
+			session.close();
+		}
+		return list;
 	}
-
 }
