4b61bd97ffad6d36b70a89d7561286b61903aee3
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 00:40:14 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/notice/controller/NoticeUpdateController.java
+++ middleProject/src/kr/or/ddit/notice/controller/NoticeUpdateController.java
@@ -17,53 +17,46 @@ import kr.or.ddit.vo.NoticeVO;
 @WebServlet("/notice/update.do")
 public class NoticeUpdateController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String noticeNoParam = request.getParameter("noticeNo");
 		int no = 0;
 		if (noticeNoParam != null && !noticeNoParam.isEmpty()) {
-		    try {
-		        no = Integer.parseInt(noticeNoParam);
-		    } catch (NumberFormatException e) {
-		       e.printStackTrace();
-		    }
+			try {
+				no = Integer.parseInt(noticeNoParam);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		
+
 		INoticeService service = NoticeServiceImpl.getInstance();
-		 
 		NoticeVO noticeVO = service.getNotice(no);
 		
 		request.setAttribute("noticeVO", noticeVO);
 		
 		request.getRequestDispatcher("/view/notice/noticeUpdateForm.jsp").forward(request, response);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		 request.setCharacterEncoding("UTF-8");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		
 		String noticeNoParam = request.getParameter("notice_no");
 		int no = 0;
 		if (noticeNoParam != null && !noticeNoParam.isEmpty()) {
-		    try {
-		        no = Integer.parseInt(noticeNoParam);
-		    } catch (NumberFormatException e) {
-		        e.printStackTrace();
-		    }
+			try {
+				no = Integer.parseInt(noticeNoParam);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
 		}
 		String title = request.getParameter("notice_title");
 		String content = request.getParameter("notice_content");
-		
-		
+
 		INoticeService service = NoticeServiceImpl.getInstance();
 		NoticeVO noticeVO = new NoticeVO(no, title, content);
 		
 		int cnt = service.updateNotice(noticeVO);
-		
-		 
-		
+
 		response.sendRedirect(request.getContextPath()+"/notice/list.do");
 	}
 
