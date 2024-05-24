03c8013a1996c99730b352a8d608a0ac9b91a995
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 00:26:38 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/member/controller/FogotId.java
+++ middleProject/src/kr/or/ddit/member/controller/FogotId.java
@@ -13,11 +13,9 @@ import kr.or.ddit.member.service.MemberServiceImpl;
 import kr.or.ddit.util.MaskingUtil;
 import kr.or.ddit.vo.MemberVO;
 
-
 @WebServlet("/member/fogotId.do")
 public class FogotId extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
@@ -32,29 +30,22 @@ public class FogotId extends HttpServlet {
 		String id = service.getID(memMail);
 		
 		if (id != null) {
-        // 메일 주소에 해당하는 아이디를 찾았을 경우
-		// 앞에 5자리 제외 *로 표현 메서드 호출
-		String maskId = MaskingUtil.maskId(id);
-		
-		//마스킹된 아이디를 request에 저장
-		request.setAttribute("id", maskId);
-		
-		 } else {
-        // 메일 주소에 해당하는 아이디를 찾지 못했을 경우
-        // 실패를 저장
-        request.setAttribute("check", "false");
-	        }
+			// 메일 주소에 해당하는 아이디를 찾았을 경우, 앞에 5자리 제외 *로 표현 메서드 호출
+			String maskId = MaskingUtil.maskId(id);
+			
+			//마스킹된 아이디를 request에 저장
+			request.setAttribute("id", maskId);
+		} else {
+			// 메일 주소에 해당하는 아이디를 찾지 못했을 경우, 실패를 저장
+			request.setAttribute("check", "false");
+		}
 		
 		// view페이지로 이동
 		request.getRequestDispatcher("/view/login_out/getId.jsp").forward(request, response);
-		
-		
-	
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
