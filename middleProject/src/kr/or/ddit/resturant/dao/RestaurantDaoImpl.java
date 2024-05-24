a2069285dd68a7650b8796d1782b59a7ffbf24f8
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 01:12:25 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/resturant/dao/RestaurantDaoImpl.java
+++ middleProject/src/kr/or/ddit/resturant/dao/RestaurantDaoImpl.java
@@ -1,7 +1,5 @@
 package kr.or.ddit.resturant.dao;
 
-
-
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,56 +9,55 @@ import kr.or.ddit.vo.RestaurantVO;
 
 public class RestaurantDaoImpl implements IRestaurantDao{	
 	private static IRestaurantDao dao;
-    private RestaurantDaoImpl() {}     
+	private RestaurantDaoImpl() {}     
 	public static IRestaurantDao getInstance() {
 		if(dao==null) dao= new RestaurantDaoImpl();
 		return dao;
-	    }  //singleton 패턴	
+	}  //singleton 패턴	
 	
 	@Override
 	public List<RestaurantVO> selectRestaurant() {
 		List<RestaurantVO> list= null;
 		SqlSession session= MybatisUtil.getSqlSession();
-			try {
-				list=session.selectList("restaurantVo.selectRestaurant");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}finally {
-					session.commit();
-					session.close();
-				}
-					return list;
+		try {
+			list=session.selectList("restaurantVo.selectRestaurant");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.commit();
+			session.close();
+		}
+		return list;
 	}//selectRes 끝
 	
-
 	@Override
 	public List<RestaurantVO> selectByDong(String dong) {
 		List<RestaurantVO> list= null;
 		SqlSession session= MybatisUtil.getSqlSession();
-			try {
-				list=session.selectList("restaurantVo.selectByDong",dong);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}finally {
-					session.commit();
-					session.close();
-				}
-					return list;
+		try {
+			list=session.selectList("restaurantVo.selectByDong",dong);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.commit();
+			session.close();
+		}
+		return list;
 	}//selectbyDong 끝
 
 	@Override
 	public List<RestaurantVO> selectByName(String name) {
 		List<RestaurantVO> list= null;
 		SqlSession session= MybatisUtil.getSqlSession();
-			try {
-				list=session.selectList("restaurantVo.selectByName",name);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}finally {
-					session.commit();
-					session.close();
-				}
-					return list;
+		try {
+			list=session.selectList("restaurantVo.selectByName",name);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.commit();
+			session.close();
+		}
+		return list;
 	}//selectbyName 끝
 
 }
