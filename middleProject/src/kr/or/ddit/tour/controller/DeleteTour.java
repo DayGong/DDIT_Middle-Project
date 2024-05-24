cc4454377fee2bb6af457dd351b9296cab0fa7f1
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 01:15:47 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/tour/controller/DeleteTour.java
+++ middleProject/src/kr/or/ddit/tour/controller/DeleteTour.java
@@ -1,7 +1,5 @@
 package kr.or.ddit.tour.controller;
 
-
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -15,33 +13,24 @@ import kr.or.ddit.culture.service.ICultureService;
 import kr.or.ddit.tour.service.ITourService;
 import kr.or.ddit.tour.service.TourServiceImpl;
 
-
-
-
 @WebServlet("/tour/deleteTour.do")
 public class DeleteTour extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	        // num가져오기 
-		      int bnum= Integer.parseInt(request.getParameter("num"));
-		    // service객체 얻기
-		      ITourService service= TourServiceImpl.getInstance();
-		    // service 메소드 호출-결과값 받기
-		      int res= service.deleteTour(bnum);
-		    // 결과값 request에 저장
-		        request.setAttribute("result", res);
-		    // view로 이동
-		    request.getRequestDispatcher("tourview/result.jsp").forward(request, response);
-		
-		
-		
-		
+		// num가져오기 
+		int bnum= Integer.parseInt(request.getParameter("num"));
+		// service객체 얻기
+		ITourService service= TourServiceImpl.getInstance();
+		// service 메소드 호출-결과값 받기
+		int res= service.deleteTour(bnum);
+		// 결과값 request에 저장
+		request.setAttribute("result", res);
+		// view로 이동
+		request.getRequestDispatcher("tourview/result.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
