62fe199c111ee8bce6adfb2858caa3b97c7512cc
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 01:16:53 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/tour/controller/TourListName.java
+++ middleProject/src/kr/or/ddit/tour/controller/TourListName.java
@@ -16,35 +16,30 @@ import kr.or.ddit.tour.service.ITourService;
 import kr.or.ddit.tour.service.TourServiceImpl;
 import kr.or.ddit.vo.TourVO;
 
-
 @WebServlet("/tour/tourListName.do")
 public class TourListName extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		   request.setCharacterEncoding("utf-8");
-		   response.setCharacterEncoding("utf-8");		   
-		   response.setContentType("application/json; charset=utf-8 ");
-		   String dong= request.getParameter("dong");
-		   TourVO vo= null;
-		   List<TourVO> list=null;
-		   ITourService service= TourServiceImpl.getInstance();
-		   Gson gson =new Gson();
-		   
-		   list= service.selectNameTour(dong);
-		   String res= gson.toJson(list); 
-		   PrintWriter out = response.getWriter();
-		   out.write(res);
-		   out.flush();
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");		   
+		response.setContentType("application/json; charset=utf-8 ");
+		String dong= request.getParameter("dong");
+		TourVO vo= null;
+		List<TourVO> list=null;
+		ITourService service= TourServiceImpl.getInstance();
+		Gson gson =new Gson();
 		
+		list= service.selectNameTour(dong);
+		String res= gson.toJson(list); 
+		PrintWriter out = response.getWriter();
+		out.write(res);
+		out.flush();
 
-		
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
