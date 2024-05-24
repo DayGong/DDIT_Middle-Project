4115222e3fbaf197f06c3f0a395398182ee7319a
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 01:11:20 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/resturant/dao/IRestaurantDao.java
+++ middleProject/src/kr/or/ddit/resturant/dao/IRestaurantDao.java
@@ -2,17 +2,13 @@ package kr.or.ddit.resturant.dao;
 
 import java.util.List;
 
-
 import kr.or.ddit.vo.RestaurantVO;
 
 public interface IRestaurantDao {
-	
          
-    public List<RestaurantVO> selectRestaurant(); 
-	
+	public List<RestaurantVO> selectRestaurant(); 
 	
 	public List<RestaurantVO> selectByDong(String dong);
-	
 	
 	public List<RestaurantVO> selectByName(String name);
 	 
