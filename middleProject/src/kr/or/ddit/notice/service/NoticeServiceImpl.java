c88e85aa51ec422247429d72efe4d2aabacc1b69
DayGong <142587274+DayGong@users.noreply.github.com>
2024-05-25 00:42:17 +0900

    [수정]줄 정렬

--- middleProject/src/kr/or/ddit/notice/service/NoticeServiceImpl.java
+++ middleProject/src/kr/or/ddit/notice/service/NoticeServiceImpl.java
@@ -13,11 +13,9 @@ public class NoticeServiceImpl implements INoticeService {
 
 	private INoticeDao noticeDao;
 	private static INoticeService noticeService;
-	
 	public NoticeServiceImpl() {
 		noticeDao = new NoticeDaoImpl();
 	}
-	
 	public static INoticeService getInstance() {
 		if(noticeService==null) {
 			noticeService = new NoticeServiceImpl();
@@ -54,54 +52,5 @@ public class NoticeServiceImpl implements INoticeService {
 	public int insertNotice(NoticeVO noticeVO) {
 		return noticeDao.insertNotice(noticeVO);
 	}
-
-//	@Override
-//	public PageVO getPageInfo(int page, String stype, String sword) {
-//		Map<String, Object> map = new HashMap<String, Object>();
-//		map.put("stype", stype);
-//		map.put("sword", sword);
-//		
-//		int count = this.getTotalCount(map);
-//		
-//		int plist = PageVO.getPerList();
-//		int totalPage = (int) Math.ceil((double)count/plist);
-//		
-//		if(page>totalPage) page = totalPage;
-//		
-//		int start = (page -1)*plist +1;
-//		int end = start + plist -1;
-//		
-//		if(end>count) {
-//			end = count;
-//		}
-//		
-//		int ppage = PageVO.getPerPage();
-//		int startPage = ((page-1)/ppage*ppage)*1;
-//		int endPage = startPage + ppage - 1;
-//		
-//		if (endPage > totalPage) {
-//			endPage = totalPage;
-//		}
-//		
-//		PageVO pageVo = new PageVO();
-//		pageVo.setStart(start);
-//		pageVo.setEnd(end);
-//		pageVo.setStartPage(startPage);
-//		pageVo.setEndPage(endPage);
-//		pageVo.setTotalPage(totalPage);
-//		
-//		return pageVo;
-//	}
-
-//	@Override
-//	public int getTotalCount(Map<String, Object> map) {
-//		return noticeDao.getTotalCount(map);
-//	}
-
-//	@Override
-//	public List<NoticeVO> selectByPage(Map<String, Object> map) {
-//		return noticeDao.getAllNotice();
-//	}
-
 
 }	//NoticeServiceImpl 끝
